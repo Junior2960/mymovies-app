812d3e350ec164bb492fe2ce6f06a1654c64580a
Junior2960 <Katongolemoses2960@gmail.com>
2024-05-30 21:24:53 +0300

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      <h1 style="text-align: center; color: red; font-size: 40px"Movie Website</h1>
+      <h1 style="text-align: center; color: red; font-size: 40px">Movie Website</h1>
       <h4 style="text-align: center; color: red; font-size: 30px">By Code World </h4>
     </div>
   );
